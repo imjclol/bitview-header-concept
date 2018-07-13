913038633aafccfb77f9105ec59b01a4b6d1ea4e
JC <37458111+imjclol@users.noreply.github.com>
2018-07-13 16:02:16 -0700

    initial

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>BitView Concept</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+
+    <center>
+
+    <!-- Heading -->
+    <div id="nav">
+        
+        <div id="nav_l"><img src="/bitview.png" alt="bv" width="120" height="48"></div>
+        
+        <div id="nav_r">
+            <b><a href="/signup">Sign Up</a></b>
+            <span>|</span>
+            <a href="/my_account">My Account</a>
+            <span>|</span>
+            <a href="/history">History</a>
+            <span>|</span>
+            <a href="/help">Help</a>
+            <span>|</span>
+            <a href="/login">Log In</a>
+            <br>
+            <form action="/search" method="get">
+                <input name="search_contents" type="text" width="280">
+                <button name="search_submit" type="submit">Search</button>
+            </form>
+        </div>
+
+        <div id="navbar">
+            <div class="tab"><a href="/videos"><p>Videos</p></a></div>
+            <div class="tab"><a href="/catergories"><p>Catergories</p></a></div>
+            <div class="tab"><a href="/channels"><p>Channels</p></a></div>
+            <div class="tab"><a href="/community"><p>Community</p></a></div>
+        </div>
+
+        <div class="hd">
+            <div id="hd_l">
+                <a href="/my_videos">My Videos</a>
+                <span>|</span>
+                <a href="/favorites">My Favorites</a>
+                <span>|</span>
+                <a href="/messages">My Messages</a>
+                <span>|</span>
+                <a href="/subscriptions">My Subscriptions</a>
+                <span>|</span>
+                <a href="/my_playlists">My Playlists</a>
+                <span>|</span>
+                <a href="/my_groups">My Groups</a>
+                <span>|</span>
+                <a href="/profile">My Profile</a>
+            </div>
+        </div>
+
+    </div>
+  
+</body>
+</html>
